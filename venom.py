0e2a04914db05d0b2798b6a867ed3c74871f72c6
😈😈😈😈😈😎KUNALBOY <ankitatikadar447@gmail.com>
2024-07-19 07:20:09 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = '7255048090:AAH2_TjHO-6Kk8WVZiwPdbZMfSPUJhwrZNY'
-ADMIN_ID = '5588464519'
+API_TOKEN = '7122197983:AAH2R4fO76qC6jGgNakZUQEIw-225wVJycQ'
+ADMIN_ID = '7245486488'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
